2f649e6d4e9ad3b7b90cb9e6bf3910fc741121a7
promtor <promerotoro@gmail.com>
2025-04-23 18:48:42 +0200

    V0.0

---
+++ spendy/src/main/java/com/spendy/Main.java
@@ -0,0 +1,10 @@
+package com.spendy;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
