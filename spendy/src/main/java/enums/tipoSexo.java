7dc29eadd4e1e34149c92f11c121ac34c7bed400
XelaJr <alex@canadasfleury.name>
2025-04-24 16:50:42 +0200

    v1.0 - Archivos inservibles borrados

--- spendy/src/main/java/enums/tipoSexo.java
+++
@@ -1,7 +0,0 @@
-package enums;
-
-public enum tipoSexo {
-	Masculino,
-	Femenino,
-	No_Definido
-}
--- spendy/src/main/java/enums/tipoTarjeta.java
+++
@@ -1,9 +0,0 @@
-package enums;
-
-public enum tipoTarjeta {
-
-	PayPal,
-	Visa,
-	Mastercard,
-	AmericanExpress
-}
