62c28cd89dee6eb2bcfeadcdaf9e58615bad4fa5
PacoBustos <fjbustosmarsal@al.uloyola.es>
2025-04-24 23:37:34 +0200

    V21PACO

--- spendy/src/main/java/dao/ManagerGastos.java
+++ spendy/src/main/java/dao/ManagerGastos.java
@@ -54,12 +54,54 @@ public class ManagerGastos {
         return res;
     }
 
+        /**
+     * Filtra gastos según parámetros: desde, hasta, min, max, categoria
+     */
     public static List<Gasto> filtrarGastos(JSONObject filtros) {
-        // TODO: filtrar por fecha, importe y categoría
-        return obtenerGastos();
-    }
-
-    public static JSONObject datosGraficoMensual() {
+        List<Gasto> res = new ArrayList<>();
+        Date fechaDesde = null, fechaHasta = null;
+        BigDecimal minImporte = null, maxImporte = null;
+        Long catId = null;
+        // Parseo de filtros si están presentes
+        if (filtros.has("desde")) {
+            fechaDesde = Date.valueOf(filtros.getString("desde"));
+        }
+        if (filtros.has("hasta")) {
+            fechaHasta = Date.valueOf(filtros.getString("hasta"));
+        }
+        if (filtros.has("min")) {
+            minImporte = BigDecimal.valueOf(filtros.getDouble("min"));
+        }
+        if (filtros.has("max")) {
+            maxImporte = BigDecimal.valueOf(filtros.getDouble("max"));
+        }
+        if (filtros.has("categoria")) {
+            catId = filtros.getLong("categoria");
+        }
+        // Filtrado
+        for (Gasto g : gastos) {
+            boolean ok = true;
+            if (fechaDesde != null && g.getFecha().before(fechaDesde)) {
+                ok = false;
+            }
+            if (fechaHasta != null && g.getFecha().after(fechaHasta)) {
+                ok = false;
+            }
+            if (minImporte != null && g.getImporte().compareTo(minImporte) < 0) {
+                ok = false;
+            }
+            if (maxImporte != null && g.getImporte().compareTo(maxImporte) > 0) {
+                ok = false;
+            }
+            if (catId != null && !g.getCategoriaId().equals(catId)) {
+                ok = false;
+            }
+            if (ok) {
+                res.add(g);
+            }
+        }
+        return res;
+    }public static JSONObject datosGraficoMensual() {
         // TODO: agrupa por mes
         return new JSONObject();
     }
